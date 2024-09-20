43bc0d6548bf8df86ff7fd4d8ac957f532176f73
Mark Outeke <64286169+Mark-Outeke@users.noreply.github.com>
2024-09-20 14:33:42 +0300

    edits to the api call for displaying data

--- src/mark/jsx/TrackedEntityDetails.jsx
+++ src/mark/jsx/TrackedEntityDetails.jsx
@@ -39,54 +39,57 @@ const TrackedEntityDetails = () => {
     return <div className="alert alert-danger">Error: {error}</div>;
   } else {
     return (
-      <div className="container mt-4">
-        <h1>Instance Details</h1>
-        {details.enrollments && details.enrollments.length > 0 ? (
-          <table className="table table-bordered table-striped">
-            <thead>
-              <tr>
-                <th>Enrollment ID</th>
-                <th>Event ID</th>
-                <th>Data Element</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {details.enrollments.map((enrollment, enrollmentIndex) => (
-                enrollment.events && enrollment.events.length > 0 ? (
-                  enrollment.events.map((event, eventIndex) => (
-                    event.dataValues.map((dataValue, dataIndex) => (
-                      <tr key={`${enrollmentIndex}-${eventIndex}-${dataIndex}`}>
-                        {dataIndex === 0 ? ( // Check if it is the first dataValue for the row
-                          <>
-                            <td rowSpan={event.dataValues.length}>{enrollment.enrollment}</td>
-                            <td rowSpan={event.dataValues.length}>{event.event}</td>
-                            <td>{dataValue.dataElement}</td>
-                            <td>{dataValue.value}</td>
-                          </>
-                        ) : (
-                          <React.Fragment> 
-                            <td>{dataValue.dataElement}</td>
-                            <td>{dataValue.value}</td>
-                          </React.Fragment>
-                          
-                        )}
-                      </tr>
+     
+        <div className="container mt-4">
+          <h1>Instance Details</h1>
+          {details.enrollments && details.enrollments.length > 0 ? (
+            <table className="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>Tracked Entity Instance ID</th>
+                  <th>Enrollment ID</th>
+                  <th>Event ID</th>
+                  <th>Data Element</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {details.enrollments.map((enrollment, enrollmentIndex) => (
+                  enrollment.events && enrollment.events.length > 0 ? (
+                    enrollment.events.map((event, eventIndex) => (
+                      event.dataValues.map((dataValue, dataIndex) => (
+                        <tr key={`${enrollmentIndex}-${eventIndex}-${dataIndex}`}>
+                          {dataIndex === 0 ? ( // Check if it is the first dataValue for the row
+                            <>
+                              <td rowSpan={event.dataValues.length}>{enrollment.trackedEntityInstance}</td> {/* Add tracked entity instance ID here */}
+                              <td rowSpan={event.dataValues.length}>{enrollment.enrollment}</td>
+                              <td rowSpan={event.dataValues.length}>{event.event}</td>
+                              <td>{dataValue.dataElement}</td>
+                              <td>{dataValue.value}</td>
+                            </>
+                          ) : (
+                            <React.Fragment>
+                              <td>{dataValue.dataElement}</td>
+                              <td>{dataValue.value}</td>
+                            </React.Fragment>
+                          )}
+                        </tr>
+                      ))
                     ))
-                  ))
-                ) : (
-                  <tr key={enrollmentIndex}>
-                    <td colSpan="4">No events found for Enrollment ID: {enrollment.enrollment}</td>
-                  </tr>
-                )
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p>No details found for this instance.</p>
-        )}
-      </div>
-    );
+                  ) : (
+                    <tr key={enrollmentIndex}>
+                      <td colSpan="5">No events found for Enrollment ID: {enrollment.enrollment}</td>
+                    </tr>
+                  )
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>No details found for this instance.</p>
+          )}
+        </div>
+      );
+      
   }
 };
 
